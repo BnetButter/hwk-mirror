a809b1dc4caa59fa08e42862726b6a9a7ad84196
BnetButter <37258095+BnetButter@users.noreply.github.com>
2019-06-10 14:02:58 -0500

    .

--- lib/data/configurations.py
+++ lib/data/configurations.py
@@ -1,6 +1,7 @@
 DEFAULT_CONFIG = {
     "POS":{},
     "Display":{},
+    "Server":{},
     "router":"192.168.1.1",
     "ip": "198.162.1.100",
     "port": "8080",
--- lib/data/directory.py
+++ lib/data/directory.py
@@ -2,6 +2,10 @@ from pathlib import Path
 import os
 
 APPDATA = os.path.join(str(Path.home()), ".hwk")
+
+if not os.path.exists(APPDATA):
+    os.mkdir(APPDATA)
+
 MENUPATH = os.path.join(APPDATA, "menu_items.json")
 LOGPATH = os.path.join(APPDATA, "server.log")
 CONFIGPATH = os.path.join(APPDATA, "config.json")
